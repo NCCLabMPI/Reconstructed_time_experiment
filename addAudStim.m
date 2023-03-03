86d46f1d1b7cbf9201597826548ec1f5c8ccebee
Micha Fabio Engeser <micha.engeser@ad.aesthetics.mpg.de>
2023-03-03 12:15:47 +0100

    Updated Sound balance

--- addAudStim.m
+++ addAudStim.m
@@ -26,14 +26,13 @@ end
 trial_mat.trial = (1:length(trial_mat.block))';
 
 % sort table
-trial_mat = sortrows(trial_mat, {'duration', 'category', 'trial_type'});
+trial_mat = sortrows(trial_mat, {'duration', 'category', 'trial_type', 'orientation'});
 
 % b) SOA between visual and auditory stimulus (1-4 for onset, 5-8 for offset)
 trial_mat.SOA = repmat([0,116,232,466],1,1440/4)';
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-trial_mat.SOA_lock = repmat({[repmat({'onset'},1,4), repmat({'offset'},1,4)]},1,1440/8)';
-%%%%%%%%%%%%%%%%%%
+SOA_lock_vec = repmat({[repmat({'onset'},1,4), repmat({'offset'},1,4)]},1,1440/8);
+trial_mat.SOA_lock = horzcat(SOA_lock_vec{:})';
 
 % c) Pitch of auditory stimulus (high pitch = 1100, low pitch = 1000 Hz)
 trial_mat.pitch = repmat([repmat(LOW_PITCH,1,8),repmat(HIGH_PITCH,1,8)],1,1440/16)';
@@ -44,46 +43,4 @@ trial_mat = sortrows(trial_mat, 'trial');
 order = {'trial','block','target_1','target_2','trial_type','category','duration','SOA','SOA_lock','pitch','orientation','identity','stim_jit'};
 trial_mat = trial_mat(:,order);
 
-
-%% balance controls
-% Since total number of trials without target (1280) cannot be divided by 3
-% the duration will be slightly imbalanced to other variables (e.g. SOA or
-% category) but this imbalance shoud not be bigger than 1
-
-% SOA to Duration of visual stimulus
-soa_dur_mat = zeros(8,3);
-for soa = 1:8
-    for dur_num = 1:3
-        durs = [0.5, 1, 1.5];
-        dur = durs(dur_num);
-        [rows, ~] = size(trial_mat(trial_mat.SOA == soa & abs(trial_mat.duration - dur) < 0.1,:));
-        soa_dur_mat(soa,dur_num) = rows;
-    end
-end
-
-% Category to Duration of visual stimulus
-cat_dur_mat = zeros(4,3);
-for cat_num = 1:4
-    cats = {'face', 'object', 'letter', 'false_font'};
-    cat = cats{cat_num};
-    for dur_num = 1:3
-        durs = [0.5, 1, 1.5];
-        dur = durs(dur_num);
-        [rows, ~] = size(trial_mat(strcmp(trial_mat.category, cat) & abs(trial_mat.duration - dur) < 0.1,:));
-        cat_dur_mat(cat_num, dur_num) = rows;
-    end
-end
-
-% pitch to Duration of visual stimulus
-pitch_dur_mat = zeros(2,3);
-for pitch_num = 1:2
-    pitchs = [1000,1100];
-    pitch = pitchs(pitch_num);
-    for dur_num = 1:3
-        durs = [0.5, 1, 1.5];
-        dur = durs(dur_num);
-        [rows, ~] = size(trial_mat(trial_mat.pitch == pitch & abs(trial_mat.duration - dur) < 0.1,:));
-        pitch_dur_mat(pitch_num,dur_num) = rows;
-    end
-end
 end
