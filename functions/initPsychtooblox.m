0c774460914bb3d2cee970a1a7372a6ddd9546ee
Alex Lepauvre <alex.lepauvre@ae.mpg.de>
2023-03-22 12:41:40 +0100

    Clean up initPsychtoolbox

--- functions/initPsychtooblox.m
+++ functions/initPsychtooblox.m
@@ -1,62 +1,111 @@
 
 
-function screenError = initPsychtooblox()
-    
+function [] = initPsychtooblox()
+    global DEBUG ScreenHeight stimSizeHeight ScreenWidth refRate screenScaler fontType fontSize fontColor text gray w REF_RATE_OPTIMAL center
+    global WINDOW_RESOLUTION debugFactor NO_FULLSCREEN  VIEWING_DISTANCE MAX_VISUAL_ANGEL STIM_DURATION TRIAL_DURATION ppd
+    global BIT_DURATION pahandle% These are required for the sound display
     disp('WELCOME to initPsychtooblox')
-% INITPSYCHTOOLBOX Initilizes Psychtoolbox and opens graphics window
-% output:
-% -------
-% * Opens a psychtoolbox window *
-% screenError - TRUE or FALSE answer if the function did not succeed in
-% opening a 100Hz window.
-
-    global DEBUG ScreenHeight stimSizeHeight ScreenWidth refRate screenScaler fontType fontSize fontColor text gray w REF_RATE_OPTIMAL center TRUE FALSE
-    global WINDOW_RESOLUTION debugFactor NO_FULLSCREEN  VIEWING_DISTANCE MAX_VISUAL_ANGEL STIM_DURATION TRIAL_DURATION MRI_BASELINE_PERIOD ppd cx cy
-    global NO_AUDIO VOLUME SAMPLE_RATE NR_CHANNELS BIT_DURATION pahandle% These are required for the sound display
-
-    PsychDefaultSetup(2);
     
-    % Set sync tests:
+    %% Set preferences and open graphic window:
+    % Set psychtoolbox default to 2
+    PsychDefaultSetup(2);
+    % Set teh screen preferences:
     try
-        if DEBUG Screen('Preference', 'SkipSyncTests', 1); else Screen('Preference', 'SkipSyncTests', 1); end
+        if DEBUG 
+            % In debug mode, skip the sync test, as it crashes on low perf
+            % laptopts
+            Screen('Preference', 'SkipSyncTests', 1); 
+
+        else
+            % For the real experiment, do not skip the sync test
+            Screen('Preference', 'SkipSyncTests', 0); 
+        end
     catch
-        if DEBUG Screen('Preference', 'SkipSyncTests', 1); else Screen('Preference', 'SkipSyncTests', 1); end
+        % Trying a second time just in case something went wrong the first
+        % try
+        if DEBUG 
+            Screen('Preference', 'SkipSyncTests', 1); 
+        else
+            Screen('Preference', 'SkipSyncTests', 0); 
+        end
     end
-   
-    % Setting the verbosity to high:
+    Screen('Preference', 'TextRenderer', 1);
     Screen('Preference', 'VisualDebugLevel', 4);
+    % Get the different screens:
+    screens = Screen('Screens');
+    % Use the last screen preferentially:
+    screenNumber = max(screens);
+    % Set the gray:
+    gray = [125,125,125];
 
-    screens         =   Screen('Screens');
-    screenNumber    =   max(screens);
-    %screenNumber   =   1;
-    %gray           =   round(GrayIndex(screenNumber)); % 128
-    gray            =   [125,125,125];
-
-    % Finding the screen size, current resolution and refresh rate:
+    % Open a graphic window:
     try
-        if NO_FULLSCREEN [w, wRect]  =  Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); else [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); end
+        if NO_FULLSCREEN 
+            % For debugging purposes, we can run the experiment with not
+            % full screen
+            [w, wRect] = Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); 
+        else
+            % But here again, for  the real experiment go for full screen
+            [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); 
+        end
     catch
-        if NO_FULLSCREEN [w, wRect]  =  Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); else [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); end
+        % Try again in case something went wrong
+        if NO_FULLSCREEN 
+            [w, wRect] = Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); 
+        else
+            [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); 
+        end
     end
+    
+    % Hide the cursor:
+    if ~NO_FULLSCREEN
+        HideCursor;
+    end
+        % this enables us to use the alpha transparency
+    Screen('BlendFunction', w, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA', [gray 128]);
+
+    % Setting priority to max, such that the experiment has full priority
+    % on the CPU:
+    priorityLevel=MaxPriority(w);
+    Priority(priorityLevel);
+    
+    %% Initialize sound parameters
+    InitializePsychSound(1);
+    device = [];
+    mode = 1;
+    reqlatencyclass = 1;
+    freq = [];
+    channels = 2;
+    pahandle = PsychPortAudio('Open', device, mode, reqlatencyclass, freq, channels);
+    % Get the status of the padhandle:
+    status = PsychPortAudio('GetStatus', pahandle);
+    sr = status.SampleRate;
+    disp(sr)
+    n_samples = BIT_DURATION/(1/sr);
+    squarewave = [ones(n_samples/2, 2); -1*ones(n_samples/2,2)];
+    % Adding the square wave to the buffer:
+    trig_buffer = PsychPortAudio('CreateBuffer', pahandle, squarewave');
+    % Load the trigger into a buffer, so that it can be played during
+    % the experiment. Because we have only one buffer, we can load it
+    % only once in the beginning:
+    PsychPortAudio('FillBuffer', pahandle, trig_buffer);
+    
+    %% Derive relevant parameters from setup information:
+    % Extract screen parameters in pixels:
     ScreenWidth     =  wRect(3);
     ScreenHeight    =  wRect(4);
     center          =  [ScreenWidth/2; ScreenHeight/2];
+    % Get hte refresh rate:
     hz = Screen('NominalFrameRate', w);
-    disp(hz);
     refRate = hz.^(-1);
-    if DEBUG == 2 refRate = refRate / debugFactor ; end
-    sca;
-    
-    %trying to set the refresh rate to optimal
-    if ~DEBUG
-        try
-            SetResolution(screenNumber,ScreenWidth,ScreenHeight,(REF_RATE_OPTIMAL));
-            screenError = FALSE;
-        catch
-            screenError = TRUE;
-        end
-    else
-        screenError = TRUE;
+    if DEBUG == 2 
+        refRate = refRate / debugFactor ;
+    end
+    % If we are not in debug mode and that the screen refresh rate does not
+    % match the expected one, abort:
+    if ~DEBUG && round(hz) ~= REF_RATE_OPTIMAL
+        sca;
+        error("Error. \nThe refresh rate is %d and not equal to %d hz. Make sure you set your screen refresh rate to match the latter!", round(hz), REF_RATE_OPTIMAL)
     end
     
     % We can now actualize the stimuli duration:
@@ -69,49 +118,22 @@ function screenError = initPsychtooblox()
     DurationCoefficients_trial = round((TRIAL_DURATION )/refRate);
     TRIAL_DURATION = DurationCoefficients_trial*refRate;
     
-    % We can now actualize the MRI baseline period:
-    % It needs to be a multiple of the frame rate:
-    DurationCoefficients_baseline = round((MRI_BASELINE_PERIOD)/refRate);
-    MRI_BASELINE_PERIOD = DurationCoefficients_baseline*refRate;
-    
     screenScaler = ScreenWidth/1920; % allows scaling so that with smaller screens, objects will be of smaller sizes (1 = Full HD)
-
-    % Opening the window for the experiment:
-    try
-        if NO_FULLSCREEN [w, wRect]  =  Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); else [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); end
-    catch
-        if NO_FULLSCREEN [w, wRect]  =  Screen('OpenWindow',screenNumber, gray, WINDOW_RESOLUTION); else [w, wRect]  =  Screen('OpenWindow',screenNumber, gray); end
-    end
-
-    if ~NO_FULLSCREEN
-        HideCursor;
-    end
 
     % Stimuli size by visual angle, based on viewing distance and physical
     % size of screen (MAKE SURE YOU INPUT THESE IN)
     stimSizeHeight = getVisualAngel(VIEWING_DISTANCE,MAX_VISUAL_ANGEL(1)); % in px
 
-    % Setting the text renderer:
-    Screen('Preference', 'TextRenderer', 1);
-    %Screen('Preference','TextEncodingLocale','UTF-8');
+    % Compute the pixel per degree of visual angle:
+    ppd = getVisualAngel(VIEWING_DISTANCE,1); % pixel per degree
 
-    % this enables us to use the alpha transparency
-    Screen('BlendFunction', w, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA', [gray 128]);
-
-    % Setting priority to max, such that the experiment has full priority
-    % on the CPU:
-    priorityLevel=MaxPriority(w);
-    Priority(priorityLevel);
-
-    %% Text params
+    %% Set text params
 
     Screen('TextFont',w, fontType);
     Screen('TextStyle', w, 0);
     Screen('TextSize', w, round(fontSize*screenScaler));
     text.Color = fontColor; %black
-    %% Fixation
-    ppd = getVisualAngel(VIEWING_DISTANCE,1); % pixel per degree
-
+    
     %% PRELIMINTY PREPATATION
     % check for Opengl compatibility, abort otherwise
     AssertOpenGL;
@@ -129,37 +151,5 @@ function screenError = initPsychtooblox()
     if ~NO_FULLSCREEN
         ListenChar(2);
     end
-    
- 
-    %% Initiate system for sound signal
-    if ~NO_AUDIO
-        InitializePsychSound(1);
-        
-        % Open Psych-Audio port, with the follow arguements
-        % (1) [] = default sound device
-        % (2) 1 = sound playback only
-        % (3) 1 = default level of latency
-        % (4) Requested frequency in samples per second
-        % (5) 2 = stereo putput
-        device = [];
-        mode = 1;
-        reqlatencyclass = 3;
-        freq = [];
-        channels = 2;
-        pahandle = PsychPortAudio('Open', device, mode, reqlatencyclass, freq, channels);
-        % Get the status of the padhandle:
-        status = PsychPortAudio('GetStatus', pahandle);
-        sr = status.SampleRate;
-        disp(sr)
-        n_samples = BIT_DURATION/(1/sr);
-        squarewave = [ones(n_samples/2, 2); -1*ones(n_samples/2,2)];
-        % Adding the square wave to the buffer:
-        trig_buffer = PsychPortAudio('CreateBuffer', pahandle, squarewave');
-        % Load the trigger into a buffer, so that it can be played during
-        % the experiment. Because we have only one buffer, we can load it
-        % only once in the beginning:
-        PsychPortAudio('FillBuffer', pahandle, trig_buffer);
-    end
-    
 
 end
