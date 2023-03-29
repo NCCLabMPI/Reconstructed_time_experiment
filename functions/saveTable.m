567e5cc34088edddca1845df9fe64af88dfdb010
AlexLepauvre <54275933+AlexLepauvre@users.noreply.github.com>
2023-03-29 19:09:08 +0200

    Improve saving

--- functions/saveTable.m
+++ functions/saveTable.m
@@ -9,21 +9,17 @@
 
 function [] = saveTable(input_table, task, blk_num)
 
-global DATA_FOLDER subID session task_type
+global DATA_FOLDER subID session
 
 % Creating the directories if they don't already exist:
-if input_table.is_practice
-    session_task = ['practice_', task_type];
-    task = sprintf('%s_practice', string(task));
-else
-    session_task = string(task);
-end
-
-dir = string(fullfile(pwd,DATA_FOLDER,['sub-', subID],session_task,['ses-',num2str(session)]));
+dir = string(fullfile(pwd,DATA_FOLDER,['sub-', subID],['ses-',num2str(session)]));
 if ~exist(dir, 'dir')
     mkdir(dir);
 end
 if isnumeric(blk_num)
+    if blk_num < 0
+        blk_num = 0;
+    end
     blk_num = num2str(blk_num);
 end
  
