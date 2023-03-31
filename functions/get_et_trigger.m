80a0eec3d3054b47d2c486dea770e5e2f52e8c3b
AlexLepauvre <54275933+AlexLepauvre@users.noreply.github.com>
2023-03-31 12:04:49 +0200

    Convert cell2char

--- functions/get_et_trigger.m
+++ functions/get_et_trigger.m
@@ -3,6 +3,6 @@ function [trigger_str] = get_et_trigger(event_type, task_relevance, duration, ca
 duration = num2str(duration);
 SOA = num2str(SOA);
 pitch = num2str(pitch);
-trigger_str = join([event_type, task_relevance, duration,category,orientation, vis_stim_id, SOA, locking, pitch], "/");
+trigger_str = char(join([event_type, task_relevance, duration,category,orientation, vis_stim_id, SOA, locking, pitch], "/"));
 end
 
