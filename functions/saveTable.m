0fea8a4bf8ac99695f1c8f0dfd1172a48131847e
michaengeser <125463198+michaengeser@users.noreply.github.com>
2023-04-19 10:55:04 +0200

    enable storing of repeted practice

--- functions/saveTable.m
+++ functions/saveTable.m
@@ -22,10 +22,30 @@ if isnumeric(blk_num)
     end
     blk_num = num2str(blk_num);
 end
- 
+
+% make files names 
 fileName_mat  = fullfile(dir, sprintf('sub-%s_ses-%d_run-%s_task-%s_events.mat', subID, session, blk_num, string(task)));
-save(fileName_mat,'input_table');
 fileName_csv  = fullfile(dir, sprintf('sub-%s_ses-%d_run-%s_task-%s_events.csv', subID, session, blk_num, string(task)));
+
+% check if they already exist and mark them aas repetitions
+repetition = 0;
+new_fileName_mat = fileName_mat; 
+while exist(new_fileName_mat, 'file') == 2
+    repetition = repetition+1;
+    new_fileName_mat = insertAfter(fileName_mat,'events',['_repetition_',num2str(repetition)]);
+end 
+fileName_mat = new_fileName_mat;
+
+repetition = 0;
+new_fileName_csv  = fileName_csv; 
+while exist(new_fileName_csv , 'file') == 2
+    repetition = repetition+1;
+    new_fileName_csv = insertAfter(fileName_csv ,'events',['_repetition_',num2str(repetition)]);
+end 
+fileName_csv  = new_fileName_csv;
+
+% save files
+save(fileName_mat,'input_table');
 writetable(input_table,fileName_csv);
 
 end
